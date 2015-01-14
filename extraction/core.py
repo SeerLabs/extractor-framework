3e1c4a7e6941a3d1692ef34dea8d1f6bc5f01535
JKillian <jasonkillian35@gmail.com>
2015-01-14 15:54:11 -0500

    Begin stubbing out some methods

--- extraction/core.py
+++ extraction/core.py
@@ -3,9 +3,29 @@ class ExtractionRunner(object):
    def __init__(self):
       self.filters = []
       self.extractors = []
+      self.runnables = []
 
-   def add_filter(filter):
-      pass
+   def add_filter(self, filter):
+      #TODO we should construct a graph of filters and extractors
+      # then if there are any circular dependencies we can throw an error
+      # we can also remove the requirement to add runnables in order
+      # these comments also apply to the add_extractor method
+      self.filters.append(filter)
+      self.runnables.append(filter)
 
-   def add_extractor(extractor):
-      pass
+   def add_extractor(self, extractor):
+      self.extractors.append(extractor)
+      self.runnables.append(extractor)
+      
+   def run(self, data):
+      results = {}
+      for runnable in self.runnables:
+         dep_results = {k: v for k,v in results if k in runnable.dependencies}
+         result = runnable.run(data, dep_results)
+         results[runnable] = result
+
+      #TODO return xml string?
+      return results
+
+   def run_from_file(self, path):
+      self.run(open(path, 'rb').read())
--- extraction/runnables.py
+++ extraction/runnables.py
@@ -2,10 +2,42 @@ class Base(object):
    def __init__(self):
       pass
 
+   def check_dep_errors(self, dependency_results):
+      deps = self.__class__.dependencies
+      filter_deps = [e for e in deps if isinstance(e, Filter)]
+      extractor_deps = [e for e in deps if isinstance(e, Extractor)]
+
+      for filter in filter_deps:
+         if not dependency_results[filter]:
+            #TODO return result saying not run because filter failed
+            return   
+
+      for extractor in extractor_deps:
+         if not dependency_results[extractor]: #check if extractor errored
+            #TODO return result saying not run becaus eextractor failed
+            return
+
+      return None
+
 class Filter(Base):
-   def filter(self, data, dependencyResults):
+   def filter(self, data, dependency_results):
       return False
 
+   def run(self, data, dependency_results):
+      dep_errors =  super(Filter, self).check_dep_errors()
+      if dep_errors:
+         return dep_errors
+
+      return self.filter(data, dependency_results)
+
+
 class Extractor(Base):
-   def extract(self, data, dependencyResults):
+   def extract(self, data, dependency_results):
       return 'TODO'
+
+   def run(self, data, dependency_results):
+      dep_errors = super(Extractor, self).check_dep_errors()
+      if dep_errors:
+         return dep_errors
+
+      return self.extract(data, dependency_results)   
