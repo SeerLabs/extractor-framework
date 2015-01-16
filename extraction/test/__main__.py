b87d212d4906606fd8b84962f02a929d4e2350ce
JKillian <jasonkillian35@gmail.com>
2015-01-16 14:32:04 -0500

    Add library to convert between xml and dicts pip wasn't installed on this machine, so relevant files just copied from: https://github.com/martinblech/xmltodict

--- extraction/test/__main__.py
+++ extraction/test/__main__.py
@@ -1,11 +1,13 @@
 import unittest
 
-from extraction.test import test_core, test_runnables
+from extraction.test import test_core, test_runnables, test_xmltodict, test_dicttoxml
 
 tests = unittest.TestLoader()
 test_arr = []
 test_arr.append(tests.loadTestsFromModule(test_core))
 test_arr.append(tests.loadTestsFromModule(test_runnables))
+test_arr.append(tests.loadTestsFromModule(test_xmltodict))
+test_arr.append(tests.loadTestsFromModule(test_dicttoxml))
 
 all_tests = unittest.TestSuite(test_arr)
 
