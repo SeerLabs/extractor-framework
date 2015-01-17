07d6b884082080142867fbe92806ce56c90924be
JKillian <jasonkillian35@gmail.com>
2015-01-17 16:44:19 -0500

    Add example extractor that calls an external process In the future their might be convienience methods to make this easier

--- extraction/test/sample.py
+++ extraction/test/sample.py
@@ -7,6 +7,7 @@ from extraction.core import ExtractionRunner
 from extraction.runnables import Filter, Extractor
 import extraction.xmltodict as xmltodict
 import re
+import subprocess
 
 class HasNumbersFilter(Filter):
    def filter(self, data, deps, dict_deps):
@@ -25,18 +26,35 @@ class EmailExtractor(Extractor):
                         data,
                         re.IGNORECASE | re.UNICODE)
 
-      xml_result = xmltodict.unparse({'result': {'email': emails}}, full_document=False)
+      xml_result = xmltodict.unparse({'result': {'email': emails}}, full_document=False, pretty=True)
       xml_result = self._wrap_xml_content(xml_result)
       return xml_result
+
+class LinesStartWithNumberExtractor(Extractor):
+   @staticmethod
+   def dependencies():
+      return [HasNumbersFilter]
+
+   def extract(self, data, deps, dict_deps):
+      process = subprocess.Popen(['awk', '/^[0-9]/ {print;}', '-'],
+                        stdout=subprocess.PIPE,
+                        stdin=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
+      (stdout, stderr) =  process.communicate(data)
+      lines = [line for line in stdout.split("\n") if line]
+      xml_result = xmltodict.unparse({'result': {'line': lines}}, full_document=False, pretty=True)
+      return self._wrap_xml_content(xml_result)
+
 
 extraction_runner = ExtractionRunner()
 extraction_runner.add_filter(HasNumbersFilter)
 extraction_runner.add_extractor(EmailExtractor)
+extraction_runner.add_extractor(LinesStartWithNumberExtractor)
 print extraction_runner.run(u'''Random data inputted with some emails bob@example.com
-      And some more text with @ signs now and then. Meet you@home@2p.m.
+523 And some more text with @ signs now and then. Meet you@home@2p.m.
       And some more stuff. howie009@yahoo.com
       jones@gmail.com fredie@emerson.retail.com
-      bobbie@ist.psu.edu and that's all the text here.''')
+123 bobbie@ist.psu.edu and that's all the text here.''')
 
 
 print extraction_runner.run(u'Silly texy with email email@example.com but no numbers')
