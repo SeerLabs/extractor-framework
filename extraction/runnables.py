9826b747f0b6ca7126081a0c887a664cd2765d97
JKillian <jasonkillian35@gmail.com>
2015-01-18 00:16:55 -0500

    Document how to override filter and extract methods better

--- extraction/runnables.py
+++ extraction/runnables.py
@@ -33,6 +33,31 @@ class Base(object):
 
 class Filter(Base):
    def filter(self, data, dep_results, dict_dep_results):
+      """
+      Override this method in Filter subclasses to define custom filtering logic
+
+      This method will be called automatically by the ExtractionRunner during the extraction process
+
+      Arguments passed in:
+         data -- the original data the extractor started with
+         dep_results -- the results of any declared dependencies in xml form
+         dict_dep_results -- the same results as the previous argument, except in dict form
+
+      This method should return a string in xml format like one of the three examples:
+        <filter type="FilterClassName"><result>fail</result></filter>
+        <filter type="FilterClassName"><result>pass</result></filter>
+        <filter type="FilterClassName"><error>Error message</error></filter>
+
+      There are various helper methods to generate these strings:
+      >>> self._filter_fail_xml()
+      <filter type="FilterClassName"><result>fail</result></filter>
+      
+      >>>  self._filter_pass_xml()
+      <filter type="FilterClassName"><result>pass</result></filter>
+
+      >>> self._error_xml('Error message')
+      <filter type="FilterClassName"><error>Error message</error></filter>
+      """
       return self._filter_fail_xml()
 
    def run(self, data, dep_results, dict_dep_results):
@@ -59,6 +84,34 @@ class Filter(Base):
 
 class Extractor(Base):
    def extract(self, data, dep_results, dict_dep_results):
+      """
+      Override this method in Extractor subclasses to define custom extraction logic
+
+      This method will be called automatically by the ExtractionRunner during the extraction process
+
+      Arguments passed in:
+         data -- the original data the extractor started with
+         dep_results -- the results of any declared dependencies in xml form
+         dict_dep_results -- the same results as the previous argument, except in dict form
+
+      This method should return a string in xml format like one of the two examples:
+        <extractor type="ExtractorClassName"><result>result body as xml</result></extractor>
+        <extractor type="ExtractorClassName"><error>Error message</error></extractor>
+
+      There are various helper methods to generate these results.
+      >>> self._error_xml('Error message') will generate a string like the second example
+      <extractor type="ExtractorClassName"><error>Error message</error></extractor>
+      
+      >>> self._extractor_result_xml('<author>Joe</author><author>James</author>')
+      <extractor type="ExtractorClassName><result>
+         <author>Joe</author><author>James</author>
+      </result></extractor>
+
+      >>> self._extractor_result_xml_from_dict({'author': ['Joe', 'James']})
+      <extractor type="ExtractorClassName><result>
+         <author>Joe</author><author>James</author>
+      </result></extractor>
+      """
       return self._extractor_result_xml('Nothing')
 
    def run(self, data, dep_results, dict_dep_results):
