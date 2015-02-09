8505b661915e955b13526a7f13c035b721c786ea
JKillian <jasonkillian35@gmail.com>
2015-02-09 16:37:31 -0500

    Adjust utils.external_process func to make more sense with more use cases

--- extraction/test/test_utils.py
+++ extraction/test/test_utils.py
@@ -7,22 +7,21 @@ class TestUtils(unittest.TestCase):
       pass
 
    def test_external_process_works(self):
-      (status, out, err) = utils.external_process('Line 1\nLine 2\nLine 3\n', ['grep', '3'])
+      (status, out, err) = utils.external_process(['grep', '3'], input_data='Line 1\nLine 2\nLine 3\n')
       self.assertEqual(status, 0)
       self.assertEqual(out, 'Line 3\n')
       self.assertEqual(err, '')
 
    def test_external_process_returns_status_code(self):
-      (status, out, err) = utils.external_process('', ['true'])
+      (status, out, err) = utils.external_process(['true'])
       self.assertEqual(status, 0)
-      (status, out, err) = utils.external_process('', ['false'])
+      (status, out, err) = utils.external_process(['false'])
       self.assertEqual(status, 1)
 
    def test_external_process_timesout(self):
-      self.assertRaises(subprocess.TimeoutExpired, utils.external_process, '', ['sleep', '3'], timeout=2)
-
+      self.assertRaises(subprocess.TimeoutExpired, utils.external_process, ['sleep', '3'], timeout=2)
       # This shouldn't timeout and thus shouldn't raise an error
-      utils.external_process('', ['sleep', '3'])
+      utils.external_process(['sleep', '3'])
 
 
 
--- extraction/utils.py
+++ extraction/utils.py
@@ -2,20 +2,22 @@ import subprocess32 as subprocess
 import threading
 import signal
 
-def external_process(input_data, process_args, timeout=None):
+def external_process(process_args, input_data='', timeout=None):
    '''
    Pipes input_data via stdin to the process specified by process_args and returns the results
 
    Arguments:
-      input_data -- the data to send
       process_args -- passed directly to subprocess.Popen(), see there for more details
+      input_data -- the data to pipe in via STDIN (optional)
+      timeout -- number of seconds to time out the process after (optional)
+        IF the process timesout, a subprocess32.TimeoutExpired exception will be raised
 
    Returns:
-      (stdout, stderr) -- a tuple of strings containing stdout and stderr data
+      (exit_status, stdout, stderr) -- a tuple of the exit status code and strings containing stdout and stderr data
 
    Examples:
-      >>> external_process("Some String\nWith Data", ['grep', 'Data'])
-      ('With Data\n', '')
+      >>> external_process(['grep', 'Data'], input_data="Some String\nWith Data")
+      (0, 'With Data\n', '')
    '''
    process = subprocess.Popen(process_args,
                               stdout=subprocess.PIPE,
