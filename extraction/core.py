f67e22b2320f677bc0de73778f48351b4845ea7a
JKillian <jasonkillian35@gmail.com>
2015-03-24 17:33:13 -0400

    Remove uneeded code

--- extraction/core.py
+++ extraction/core.py
@@ -4,29 +4,8 @@ import os
 import logging
 import multiprocessing as mp
 import xml.etree.ElementTree as ET
-import copy_reg
-import types
 from extraction.runnables import *
 import extraction.utils as utils
-
-
-def _pickle_method(method):
-   func_name = method.im_func.__name__
-   obj = method.im_self
-   cls = method.im_class
-   return _unpickle_method, (func_name, obj, cls)
-
-def _unpickle_method(func_name, obj, cls):
-   for cls in cls.mro():
-      try:
-         func = cls.__dict__[func_name]
-      except KeyError:
-         pass
-      else:
-         break
-   return func.__get__(obj, cls)
-
-copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
 
 class ExtractionRunner(object):
    def __init__(self):
